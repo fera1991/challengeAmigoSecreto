90f1ede6bc5acba30d2cafb89131169f8c8ed7d0
fera1991 <00120520@uca.edu.sv>
2025-03-17 20:44:42 -0600

    Agrega: Implementacion de la funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let listaNombres = []
+let listaNombres = [];
+
+function agregarAmigo() {
+
+    let nombreAmigo = document.getElementById("amigo").value;
+    
+    console.log(nombreAmigo);
+
+    if(nombreAmigo != ""){
+        listaNombres.push(nombreAmigo);
+    }
+    else{
+        alert("No dejes vacio el en la entrada de texto");
+    }
+    
+}
