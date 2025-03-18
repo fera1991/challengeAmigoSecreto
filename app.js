441c2e1255e43a68d625f19405daa7974ab4f896
fera1991 <00120520@uca.edu.sv>
2025-03-17 21:50:02 -0600

    Agrega: Se agrego comentarios al codigo.

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ function agregarAmigo() {
     //Verifica si el input no esta vacio.
     if(nombreAmigo != ""){
         //agrega el nombre del amigo a la lista.
+        document.getElementById("resultado").innerText = "";
         listaNombres.push(nombreAmigo);
         nombreAmigo = "";
         renderizarLista();
@@ -27,9 +28,11 @@ function agregarAmigo() {
 }
 
 function renderizarLista() {
+    //Se obtiene la el elemento ul y se limpia para renderizarlo al completo, se puede reutilizar este rederizado al llamar al ganador para limpiar  la lista.
     let lista = document.getElementById("listaAmigos")
     lista.innerHTML = "";
 
+    //Se recorre la lista de amigos para agregarlos cada uno como un hijo de elemento ul y renderizarlo como un elemento li.
     listaNombres.forEach( nombre => {
         let elemento = document.createElement("li");
         elemento.textContent = nombre; 
@@ -40,13 +43,25 @@ function renderizarLista() {
 
 function sortearAmigo(){
     
-    let numeroElegido = Math.floor(Math.random()*listaNombres.length);
-    let amigoSorteado = listaNombres[numeroElegido];
-
-    let rederizarGanador = document.getElementById("resultado");
-    rederizarGanador.innerText = `El amigo secreto sorteado es: ${amigoSorteado}`;
-
-    listaNombres = [];
-    renderizarLista();
+    //Verificacion que la lista de nombre de amigos no este vacia.
+    if(listaNombres.length != 0){
+        //Se obtiene un numero aleatorio segun la cantidad de elementos de la lista.
+        let numeroElegido = Math.floor(Math.random()*listaNombres.length);
+        let amigoSorteado = listaNombres[numeroElegido];
+        
+        //Se renderisa el nombre del ganador.
+        let rederizarGanador = document.getElementById("resultado");
+        rederizarGanador.innerText = `El amigo secreto sorteado es: ${amigoSorteado}`;
+        
+        //Se limpia la lista para que solo se muestre el ganador.
+        listaNombres = [];
+        renderizarLista();
+    }
+    else{
+        //Mensaje al usuario para que agrege un nombre a la lista.
+        alert("Debes agregar al menos un amigo para participar en el sorteo.")
+    }
 
 }
+
+
